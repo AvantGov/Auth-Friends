f7dd0a5cb9f20bdf0aa58c276a35ce6a36942027
Alex <alexcouts18@gmail.com>
2020-07-12 18:50:05 -0500

    set up login logic to communicate with server and handle response message

---
+++ client/src/Components/LoginForm.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+const LoginForm = () => {
+
+    //decalring state of component
+    const [userInput, setUserInput] = useState({})
+
+    //handling input changes 
+    const handleChange = (event) => { 
+        event.preventDefault()
+        setUserInput({
+            ...userInput,
+            [event.target.name]: event.target.value
+        })
+        console.log('look here for locale state change:', userInput)
+    }
+
+
+    //handling submission logic for login:
+    const login = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:5000/api/login', userInput)
+            .then((response) => {
+                console.log('look here for the login response', response);
+                localStorage.setItem('token', response.data.payload)
+                setUserInput({})
+            })
+            .catch((error) => {
+                console.log('look here for the error messages on the login:', error)
+            })
+    }
+
+
+    //sending login request to the server 
+
+    //handling promise reponse 
+
+
+    return(
+        <div className='login-form-container'>
+            <h2> User Login: </h2>
+            <label htmlFor='username'>Username</label>
+            <input 
+                className='login-form-container__input'
+                type='text'
+                name='username'
+                // value={}
+                onChange={handleChange}
+            />
+            <label htmlFor='password'>Password</label>
+            <input 
+                className='login-form-container__input'
+                type='text'
+                name='password'
+                // value={}
+                onChange={handleChange}
+            />
+            <button 
+                onClick={login}
+                className='login-form-container__button'
+            >Login</button>
+        </div>
+    )
+}
+
+export default LoginForm;
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/CSS/index.css';
+// import '../src/CSS/index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <Router>
+
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>,
+  ,
   document.getElementById('root')
 );
 
